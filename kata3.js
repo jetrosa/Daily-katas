11034a53566f5a7dcc4c822680c951fbf8e9cb02
jetrosa <47974441+jetrosa@users.noreply.github.com>
2023-01-17 12:05:58 +0200

    kata3.js minor format changes

--- kata3.js
+++ kata3.js
@@ -2,25 +2,20 @@
  * Given a string, return if a given letter always appears immediately 
  * before another given letter
  */
-const bestFriend = (testString, a, b) => {
+const bestFriend = (str, a, b) => {
   let orderTrue = 0; //counter for the matching ordered letter pairs
-  for (let i = 0; i < testString.length - 1; i++) {
-    if (a === testString[i]) {
-      if (b === testString[i + 1]) {
+  for (let i = 0; i < str.length - 1; i++) {
+    if (a === str[i]) {
+      if (b === str[i + 1]) {
         orderTrue++;
       } else {
         return false; //return false if the second letter does not match
       }
     }
   }
-
   //return true if the letters matched in the right order at least once
-  if (orderTrue > 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
+  return orderTrue > 0 ? true : false;
+}
 
 console.log(bestFriend("he headed to the store", "h", "e")); //true
 console.log(bestFriend("i found an ounce with my hound", "o", "u")); //true
