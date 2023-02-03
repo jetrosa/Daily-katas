a0471e877639b7d6a259d5e65d2a44029aca6593
jetrosa <47974441+jetrosa@users.noreply.github.com>
2023-02-03 09:40:25 +0200

    kata13 extra solution

--- kata13.js
+++ kata13.js
@@ -8,10 +8,14 @@ const existsHigher=(arr, num)=>{
     return false;
 }
 
-console.log(existsHigher([5, 3, 15, 22, 4], 10));// ➞ true
-console.log(existsHigher([1, 2, 3, 4, 5], 8));// ➞ false
-console.log(existsHigher([4, 3, 3, 3, 2, 2, 2], 4));// ➞ true
-console.log(existsHigher([-10, -99, -57, -4], -4));// ➞ true
-console.log(existsHigher([5], 5));// ➞ true
-console.log(existsHigher([99, 99], 99));// ➞ true
-console.log(existsHigher([], 5));// ➞ false
+const existsHigher2=(arr, num)=>{
+    return (arr.some((n) => n >= num));
+}
+
+console.log(existsHigher2([5, 3, 15, 22, 4], 10));// ➞ true
+console.log(existsHigher2([1, 2, 3, 4, 5], 8));// ➞ false
+console.log(existsHigher2([4, 3, 3, 3, 2, 2, 2], 4));// ➞ true
+console.log(existsHigher2([-10, -99, -57, -4], -4));// ➞ true
+console.log(existsHigher2([5], 5));// ➞ true
+console.log(existsHigher2([99, 99], 99));// ➞ true
+console.log(existsHigher2([], 5));// ➞ false
