c19c0fa660d8195c3e27fa109699ab09420cff74
jetrosa <47974441+jetrosa@users.noreply.github.com>
2023-02-06 10:05:35 +0200

    kata14

--- kata14.js
+++ kata14.js
@@ -1,6 +1,15 @@
-const getLength = arr => arr.flat(Infinity).length;
+const getLength = (array) => {
+    let count = 0;
+  
+    array.forEach(x => {
+        count += (Array.isArray(x) ? getLength(x) : 1)
+    })
+    return count
+  }
 
-console.log(getLength([1, [2, 3]]));// ➞ 3
+//const getLength = arr => arr.flat(Infinity).length;
+
+console.log(getLength([]));// ➞ 3
 console.log(getLength([1, [2, [3, 4]]]));// ➞ 4
 console.log(getLength([1, [2, [3, [4, [5, 6]]]]]));// ➞ 6
 console.log(getLength([1, [2], 1, [2], 1]));// ➞ 5
